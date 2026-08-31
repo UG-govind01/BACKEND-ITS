d2d2e871094a2b7d778e94573f4dfff704945446
Naitik Raj <naitikrajj01@gmail.com>
2026-08-31 09:12:54 +0530

    path module

---
+++ Path__module/App.js
@@ -0,0 +1,5 @@
+const path = require("path");
+const filepath = "C:\\Users\\LENOVO\\Desktop\\BACKEND-ITS\\Path__module\\code.text"
+console.log(path.basename(filepath));
+console.log(path.dirname(filepath));
+console.log(path.extname(filepath));
--- module/buildinmodule.js
+++ module/buildinmodule.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+//const fs = require("fs");
         //// created file code 
 // fs.writeFileSync("createdByCoreModule.js", "This file is created using the fs core module.");
 
@@ -14,8 +14,27 @@ const fs = require("fs");
 //console.log("File Updated");
 
 ///// this is deleted 
-const fs = require("fs");
+//const fs = require("fs");
 
-fs.unlinkSync("demo.txt");
+//fs.unlinkSync("demo.txt");
 
-console.log("File Deleted");
+//console.log("File Deleted");
+
+ const fs = require("fs");
+
+//  fs.writeFileSync("Govind.js","this is govind");
+//  console.log("this is a ");
+//  data = fs.readFileSync("Govind.js","utf-8")
+//  console.log(data);
+//  fs.writeFileSync("Govind.js","upadateded")
+//  console.log("file upadateded successfully");
+//  fs.unlinkSync("Govind.js")
+//  console.log("file deleted successfully");
+//  fs.writeFileSync("index.html","this is html file");
+//  console.log("file createded successfylly");
+ 
+
+ data = fs.readFileSync("index.html","utf-8")
+console.log(data);
+
+ 
